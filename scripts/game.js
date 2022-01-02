7a991b76c6560b766662535fb773e378b9669847
PratimaGurav <prtmgrv2@gmail.com>
2022-01-02 15:00:16 +0000

    add tests

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,7 @@ let game = {
     currentGame: [],
     playerMoves: [],
     score: 0,
+    turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"]
 };
 
@@ -16,7 +17,18 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    //showTurns();
+    showTurns();
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
 }
 
 function lightsOn(circ) {
@@ -30,4 +42,4 @@ function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
